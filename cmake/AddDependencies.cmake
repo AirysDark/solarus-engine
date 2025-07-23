9bc0907bc4f219a9f3cc5293cb8b96b9d90e85c7
AirysDark <megamanx2sonic@gmail.com>
2025-07-23 21:08:32 +1000

    tagv2.7

--- cmake/AddDependencies.cmake
+++ cmake/AddDependencies.cmake
@@ -1,29 +1,11 @@
-# This file adds the necessary dependencies of the Solarus library.
 
-# Whether LuaJIT should be used instead of vanilla Lua.
-option(SOLARUS_USE_LUAJIT "Use LuaJIT instead of default Lua (recommended)" ON)
-
-# Find dependencies.
-set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake/modules/")
-#find_package(OpenGL)
-find_package(SDL2 REQUIRED)
-find_package(SDL2_image REQUIRED)
-find_package(SDL2_ttf REQUIRED)
-find_package(OpenAL REQUIRED)
-find_package(Vorbis REQUIRED)
-find_package(VorbisFile REQUIRED)
-find_package(Ogg REQUIRED)
-find_package(ModPlug REQUIRED)
-find_package(PhysFS REQUIRED)
-if(SOLARUS_USE_LUAJIT)
-  find_package(LuaJit REQUIRED)
-else()
-  find_package(Lua51 REQUIRED)
-endif()
-
-# Explicit link to libdl is needed for Lua on some systems.
-find_library(DL_LIBRARY dl)
-if("${DL_LIBRARY}" MATCHES DL_LIBRARY-NOTFOUND)
-  set(DL_LIBRARY "")
-endif()
-
+find_package(OpenAL REQUIRED MODULE)
+find_package(PhysFS REQUIRED MODULE)
+find_package(SDL2 REQUIRED MODULE)
+find_package(SDL2_image REQUIRED MODULE)
+find_package(SDL2_mixer REQUIRED MODULE)
+find_package(SDL2_ttf REQUIRED MODULE)
+find_package(OpenGL REQUIRED MODULE)
+find_package(GLEW REQUIRED MODULE)
+find_package(GLM REQUIRED MODULE)
+find_package(TinyXML2 REQUIRED MODULE)
